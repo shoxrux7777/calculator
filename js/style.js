ced889fbefdbf3e7ad02147edad216f742798ff2
= <shoxrux61204@gmail.com>
2023-08-04 17:51:18 +0500

    add calculator

---
+++ js/style.js
@@ -0,0 +1,7 @@
+let firstnum = prompt("Birinchi sonni kiriting... ");
+let secondnum = prompt("Ikkinchi sonni kiriting... ");
+
+console.log("Yig'indisi " + (Number(firstnum) + Number(secondnum)) + ".")
+console.log("Ayirmasi " + (Number(firstnum) - Number(secondnum)) + ".")
+console.log("Ko'paytmasi " + (Number(firstnum) * Number(secondnum)) + ".")
+console.log("Bo'linmasi " + (Number(firstnum) / Number(secondnum)) + ".")
